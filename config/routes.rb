dc80f9752a8a73bb12d9431f9e7738460f8fa3bf
marcotsui2003 <marcotsui2003@yahoo.com>
2016-03-03 16:52:48 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :create]
+  get 'students/new', to: 'students#new', as: 'new_student'
+  get '/students/:id', to: 'students#show', as: 'student'
 end
