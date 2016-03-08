4296db967a0e04bb6e95710ffe9db0e31a68d106
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-03-08 23:10:00 +0000

    completed.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,14 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new 
+  end
+  
+  def create
+    binding.pry
+    @student = params
+    redirect_to new_student_path(:student => @student)
+  end
 
 end
