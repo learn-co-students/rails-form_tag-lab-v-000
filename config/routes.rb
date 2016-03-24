c6d840c3659f40e7f38174c8b570db5dd5515a56
Brian Holland <brian.holland@proatria.com>
2016-03-23 20:49:17 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+  get '/student/new', to: 'students#new', as: 'new_student'
+  post '/students', to: 'students#create', as: 'create_student'
+  get '/students', to: 'students#index', as: 'index_students'
   get '/student/:id', to: 'students#show', as: 'student'
 end
