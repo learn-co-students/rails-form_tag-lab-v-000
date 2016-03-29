06fd714191ca7af03d1ea56ea8749b0497b7ea7c
fiveinfinity <jlachance1@gmail.com>
2016-03-29 14:41:23 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
+  def new
+  end
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = params
+    redirect_to new_student_path(student: @student)
   end
 
 end
