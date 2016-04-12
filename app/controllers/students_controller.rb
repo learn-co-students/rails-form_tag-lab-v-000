203d2e3cc04382faf7172e59b14db5e20ed40f72
viparthasarathy <viparthasarathy@gmail.com>
2016-04-12 11:57:13 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    redirect_to (@student.save ? student_path(@student) : new_student_path)
+  end
+
+  private
+
+  def student_params
+    params.permit(:first_name, :last_name)
   end
 
 end
