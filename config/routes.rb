cbf34db6976e05d3ad532aa63834b8bb4e34a711
ed-karabinus <ejkarabin@gmail.com>
2016-04-14 20:17:23 -0500

    Finished Rails form_tag lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:new, :create, :index]
   get '/student/:id', to: 'students#show', as: 'student'
 end
