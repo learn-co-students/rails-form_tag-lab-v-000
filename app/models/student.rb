2fd33a378c0cca935e6b744a855311470bde0e2e
ed-karabinus <ejkarabin@gmail.com>
2016-04-14 20:18:49 -0500

    Removed commented-out code from solution

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,5 @@
 class Student < ActiveRecord::Base
   def to_s
-    # if self.first_name && self.last_name
-    #   self.first_name + " " + self.last_name
-    # elsif self.first_name
-    #   self.first_name
-    # elsif self.last_name
-    #   self.last_name
-    # else
-    #   ""
-    # end
     self.first_name + " " + self.last_name
   end
 end
