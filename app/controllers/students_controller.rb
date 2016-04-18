c9f0cd69d22797f04bb414963122c77c00ff010f
Jesse <rocketNeck@users.noreply.github.com>
2016-04-18 13:09:30 -0400

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,13 +7,17 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
-
+  
   def new
   end
-
+  
   def create
-    @student = params
-    redirect_to new_student_path(student: @student)
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
 
+  private
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
