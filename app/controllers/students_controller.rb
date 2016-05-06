78cfcbd8562a205abb4058d762a52405be27b476
McN33ly <rmcneely23@gmail.com>
2016-05-06 16:21:22 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
+require 'pry'
+
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def create
+    session[:student] = params[:first_name] + " " + params[:last_name]
+    redirect_to new_student_path
   end
 
 end
