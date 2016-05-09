10d66f9f705b93100abe6b9f6fb8ef81861f8a92
ShiLi007 <lishidaniel@gmail.com>
2016-05-09 00:46:45 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
+  end
+
 end
