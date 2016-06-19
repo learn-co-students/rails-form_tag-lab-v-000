a431602626a42d1cb8ace0a259c2c51c16c8d94e
Vannak Chhay <vannak.chhay1@gmail.com>
2016-06-18 22:06:14 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
-  
+  before_action :set_student, only: :show
+
+  def new
+  end
+
+  def create
+    @student = params
+    redirect_to new_student_path(post: @student)
+  end
+
   def index
-    @students = Student.all
+    @student = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
