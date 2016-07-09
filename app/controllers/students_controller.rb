b3842e22bfdc71a17d58ac041c21b33c8cf48f0c
kaileighrose <kaileighrose@gmail.com>
2016-07-08 22:35:34 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @student = params
+    redirect_to new_student_path(post: @student)
+  end
+
 end
