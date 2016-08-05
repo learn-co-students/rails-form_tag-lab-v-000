0435a5dec1ceb1a9d29bcf074453fa552b2af183
miclp <michaelpeimani@gmail.com>
2016-08-05 13:07:29 -0700

    Add create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :new, :show]
+  resources :students, only: [:index, :new, :create, :show]
   get '/student/:id', to: 'students#show', as: 'student'
 end
