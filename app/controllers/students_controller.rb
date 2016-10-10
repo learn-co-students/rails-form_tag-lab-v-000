061bf5d42c7059b0e1f9b1c12a97f212de76b098
Samuel Lair <lair001@gmail.com>
2016-10-09 23:28:08 -0400

    Update students_controller.rb
    fixed accidental edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:first_name], last_name: params[:last_nameexi])
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
 end
