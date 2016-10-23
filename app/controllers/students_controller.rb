f2ccb714da0ccb64d613a1dbcb5a61e5630979cc
Bob Habig <rfhab2@gmail.com>
2016-10-23 16:34:21 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,15 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path
   end
 
   def show
