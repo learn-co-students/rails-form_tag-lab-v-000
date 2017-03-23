cda658d9ae869b133855001c9584d7fd75e6f58c
Chenxi Wu <wuchenxi2013@gmail.com>
2017-03-23 21:54:57 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    s=Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(s)
+  end
+
+
 end
