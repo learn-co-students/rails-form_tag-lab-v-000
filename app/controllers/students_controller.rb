88bd9e2f2cf62f4f56d717711114635b6fdc3af1
Erika Hughes <ehughes@knights.ucf.edu>
2017-05-09 18:28:10 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+
   end
 
 end
