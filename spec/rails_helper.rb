f4ee933a822f6da96f2cc595ce7350653d8905fc
Zain <zain@halai.us>
2017-08-10 22:25:46 -0500

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,6 +46,9 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
+  config.color = true
+  config.formatter = :documentation
   
   config.infer_spec_type_from_file_location!
 end
