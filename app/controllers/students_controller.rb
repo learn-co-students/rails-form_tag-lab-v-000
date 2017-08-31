7292fa75bfea1dbf78bd9ffbc1aaf49c2ff96344
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-31 13:54:09 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+
+  end
+
+  def new
+  end
+
+  def create
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
 
   def show
