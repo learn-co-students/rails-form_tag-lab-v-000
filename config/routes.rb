6a9f846bb8f4f8790ca8b0b222432c086030bcb9
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2017-10-09 00:26:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index , :new, :create]
+  # Accessing this path dynamically, because can't incude the :id as a value
   get '/student/:id', to: 'students#show', as: 'student'
 end
