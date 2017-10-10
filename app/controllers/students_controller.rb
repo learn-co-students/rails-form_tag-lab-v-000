b3d87fe6ad2d45c85ec39d7218ea52e392d12b45
NickisKnowledge <lifechangingstuff@gmail.com>
2017-10-10 22:48:45 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,25 @@
 class StudentsController < ApplicationController
-  
+  before_action :find_student, only: :show
   def index
     @students = Student.all
   end
 
   def show
+    @student
+  end
+
+  def new
+  end
+
+  def create
+    student = Student.create(first_name: params[:first_name],
+                   last_name: params[:last_name])
+
+    redirect_to student
+  end
+
+  private
+  def find_student
     @student = Student.find(params[:id])
   end
 
