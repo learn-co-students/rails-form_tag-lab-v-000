f5bd28bf325b12479bef0d4b34580aa6810dd8e7
johanesteves <jsesteves91@gmail.com>
2017-11-12 11:21:51 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    # displays form to submit a new student
+  end
+
+  def create
+    # POST request comes here, create new student and redirects to index page
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+
+    redirect_to students_path
+  end
 end
