4949f4446c177667ae1a21ba154cbd11077d3d3c
github username <github email address>
2017-11-18 14:53:45 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :new, :show]
+  # get '/students/new' => 'students#new', as: "new_student"
+  # get '/students/:id', to: 'students#show', as: 'student'
+  post '/students' => 'students#create'
 end
