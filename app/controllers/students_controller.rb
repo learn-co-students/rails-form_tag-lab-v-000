ccdb903b3b155cb27dbedc54737be64b5dd40d1f
github username <github email address>
2017-11-22 12:10:47 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
 
   def index
     @students = Student.all
@@ -15,5 +16,11 @@ class StudentsController < ApplicationController
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path
   end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 
 end
