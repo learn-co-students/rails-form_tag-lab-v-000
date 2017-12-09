781f6a873fbf391af4f7175f69c397c580530048
Adam Tropp <adt6261@gmail.com>
2017-12-09 17:24:58 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(title: params[:post][:title], description: params[:post][:description])
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to students_path
   end
 end
