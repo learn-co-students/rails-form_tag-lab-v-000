47003fda30ccab52715a3bfb95887f988cd4550c
miriam <miriam5000@gmail.com>
2017-12-27 21:19:03 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,18 +5,21 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def show
-   #implicit 
-  end
-
-  def new
-  	# automatic
-  end 
 
   def create
   	session[:form_params] = params.inspect
   	redirect_to new_student_path
   end 
+
+  def new
+  	# automatic
+  end 
+  
+  def show
+   #implicit 
+  end
+
+
 
 
   private
