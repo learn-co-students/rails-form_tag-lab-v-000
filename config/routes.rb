cf6ff4dfbeb5692d411128e3ff468e8c25dd9506
Chris Boujoukos <github email address>
2018-01-29 09:09:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+  get '/students/new' => 'students#new', as: 'new_student'
+  post '/students', to: 'students#create'
+
   get '/students/:id', to: 'students#show', as: 'student'
 end
