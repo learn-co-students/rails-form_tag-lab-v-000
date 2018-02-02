5dbddd10591ffa6011f8e8ae84700119f4454e0b
Alex <github email address>
2018-02-01 19:48:45 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,4 +15,21 @@ class StudentsController < ApplicationController
       redirect_to student_path(@student)
   end
 
+  #note that another way of doing new.html.erb is
+  #<%= form_tag students_path do%>
+  #      <label>First Name:</label></br>
+  #      <%= text_field_tag :"student[first_name]"%><br>
+
+  #      <label>Last Name:</label></br>
+  #      <%= text_field_tag :"student[last_name]"%><br>
+
+  #      <%= submit_tag "Submit Student"%>
+  #<%end%>
+
+  #this will also change the create action in student controller to
+      #def create
+        #@student=  Student.create(params[:student])
+          #redirect_to student_path(@student)
+        #end
+
 end
