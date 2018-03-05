b38305fdc29a1f55bd007353a05a1ed92ee602f1
churchwerks <steven.lennis@icloud.com>
2018-03-05 09:23:28 -0600

    All tests passing.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,12 +8,8 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-
-
   def create
-    #byebug
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    #byebug
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to students_path
   end
 end
