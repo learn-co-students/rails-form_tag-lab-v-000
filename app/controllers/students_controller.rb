cf1ea31f18e1c59c24284ed1663fd1cd955d8bbd
ronald vilorio <soccer23415@gmail.com>
2018-03-27 21:38:52 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,13 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+  def new
   end
 
 end
