f777b7b9346585e06ec7e2ea1bc6165293a6ee87
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-23 12:17:18 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+
 class StudentsController < ApplicationController
   
   def index
@@ -7,5 +8,17 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new 
+  end 
+
+  def create 
+
+  	# raise params.inspect
+  	# Student.create(params[:student])
+  	@first_name = params[:first_name]
+  	@last_name = params[:last_name]
+  	redirect_to new_student_path 
+  end 
 
 end
