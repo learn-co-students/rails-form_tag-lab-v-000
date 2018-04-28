9cbdef406e77eb6bba6de152b302da5c13c1ef2d
Eden <emesfun@outlook.com>
2018-04-27 21:19:03 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,4 +11,10 @@ class StudentsController < ApplicationController
   def new 
   end
 
+  def create 
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+
+    redirect_to students_path
+  end
+
 end
