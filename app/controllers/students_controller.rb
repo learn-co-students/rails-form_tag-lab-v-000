59a69ade70f081bb80608bfff15aa80d9b14b50d
AlexFriedland <friedlandaj@gmail.com>
2018-05-18 15:56:05 +0200

    migrate

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,24 @@
 class StudentsController < ApplicationController
-  
+  #runs before controller actions.
+  before_action :set_student, only: :show
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    session[:params] = params.inspect
+    redirect_to new_student_path
+  end
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
