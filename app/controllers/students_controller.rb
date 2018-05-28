01973fe92f0fa066d0d47d62aa70b339d6a6126e
wali89 <wali.rahman89@yahoo.com>
2018-05-28 15:51:21 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
+require "pry"
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to @student
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
