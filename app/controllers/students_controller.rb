de0ad8c709a2ece261c8fbd61d41086e9eaff655
arianaberger <ariana.berger@gmail.com>
2018-06-29 19:37:55 -0400

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def create
+    render new_student_path
   end
 
 end
