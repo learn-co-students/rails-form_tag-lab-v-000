2a9c55786c76b09d9a8a2d4919fb8420f2daf2c9
Lucy Wall <lhwall@gmail.com>
2018-07-04 00:54:25 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    #byebug
+    @student = Student.create!("first_name"=>"Margaery", "last_name"=>"Tyre
+ll")
   end
 
 end
