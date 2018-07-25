ec8decf262535a4a2887ea22911cb4434cf4331c
taryn yaeger <tsyaeger@gmail.com>
2018-07-25 00:58:11 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,8 +9,8 @@ class StudentsController < ApplicationController
   end
   
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to posts_path
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path
   end
 
 end
