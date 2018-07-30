aa9e056bb93c6e3f27e28a25574f7aa395a0dd77
kreopelle <kreopelle@gmail.com>
2018-07-30 13:05:53 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-  end 
+    render :new
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :new, :create]
+  resources :students, only: [:index, :create]
+  get '/students/new', to: 'students#new', as: 'new_student'
+
+  post '/students/new', to: 'students#create'
+
   get '/students/:id', to: 'students#show', as: 'student'
+
 end
