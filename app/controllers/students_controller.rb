b91e566f3ab83f0b46117a332344d3513cadd29c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-31 17:08:10 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to student_path
+    session[:form_params] = params.inspect
+      redirect_to new_student_path
   end
 
 end
