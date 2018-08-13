54bd138592c7aaf8a6356a095e74d5a6f3fe0cfe
sgibson47 <sgibson47@gmail.com>
2018-08-13 13:26:55 -0500

    add a create action to StudentsController that creates a new student using the values from params and redirects to the index page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,4 +11,9 @@ class StudentsController < ApplicationController
   def new 
   end
 
+  def create
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path
+  end
+
 end
