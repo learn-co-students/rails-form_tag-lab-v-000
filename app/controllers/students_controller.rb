0def5a1c06d80dd9678a27a4c9c7a73ad511e5e1
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-08-15 19:03:15 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,13 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path
+  end
 
 end
