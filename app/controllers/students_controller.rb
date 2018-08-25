59f1fa596fbbdbb83c2fb7bbb717bd7f8073bb92
theresamorelli <theresa.l.morelli@gmail.com>
2018-08-25 18:06:18 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,10 @@
 class StudentsController < ApplicationController
-  
+  before_action :set_student, only: :show
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
@@ -16,4 +15,9 @@ class StudentsController < ApplicationController
     redirect_to students_path
   end
 
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
