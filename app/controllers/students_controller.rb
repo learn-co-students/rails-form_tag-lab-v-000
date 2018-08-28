7f790234793b74515d5045e56af97669ba56ff99
Pan Cheng <pchengeducation@hotmail.com>
2018-08-28 01:07:29 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,15 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+    @student = params
+    redirect_to new_student_path(student: @student)
   end
 
   def show
