d7a5bbf42b08cb5df847056596984c6859950650
Ryan Manchester <ryanm17@gmail.com>
2018-09-21 19:07:18 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,12 +4,18 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def show
-    @student = Student.find(params[:id])
+  def new
   end
 
-  def new
+  def create
 
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  
+    redirect_to new_student_path
+  end
+
+  def show
+    @student = Student.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
   get '/students/new', to: 'students#new', as: 'new_student'
+  post '/students/new', to: 'students#create'
+  get '/students/:id', to: 'students#show', as: 'student'
+
 end
