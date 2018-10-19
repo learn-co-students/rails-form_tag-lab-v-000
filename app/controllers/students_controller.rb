1d93411c0deef19a0b1c9f8f5884cef77e34635b
Ludmilla Aires Negri <ludaires@gmail.com>
2018-10-18 22:48:28 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
   
+  # CReding-UD
   def index
     @students = Student.all
   end
 
+  # CRUD - REDING 
   def show
     @student = Student.find(params[:id])
+  end
+
+  # CREATE - RUD 
+  # RENDER FORM
+  def new
+  end
+
+  def create 
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path(@student)
   end
 
 end
