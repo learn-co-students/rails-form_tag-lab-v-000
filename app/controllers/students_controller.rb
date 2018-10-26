f207a212da90b9fa3e67afc47f633bbf22cf57d4
Josely Castro <joselycas@gmail.com>
2018-10-26 17:03:38 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to "/students/#{@student.id}"
   end
 
 end
