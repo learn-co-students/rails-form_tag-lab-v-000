136d92648c6c91fd3ff9ffeb583ad3b7a80edb62
Harley Rayner <harleymteden@gmail.com>
2019-01-11 22:51:00 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,14 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new 
+  end 
+
+  def create 
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to student_path(@student)
+  end 
+
   def show
     @student = Student.find(params[:id])
   end
