4209ca54ae0bd0e68fd45cf98ab2b94cece43262
esugano <esugano.git@gmail.com>
2019-05-24 15:15:46 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,15 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
+  end
+
+  def create
+    Student.create(title: params[:student][:first_name], description: params[:student][:last_name])
+    redirect_to students
   end
 
   def show
