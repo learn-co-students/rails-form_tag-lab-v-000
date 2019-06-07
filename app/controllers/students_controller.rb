156c6d88047e3cd52f8fb1578d6c68fec430a21b
github username <github email address>
2019-06-06 21:43:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
 
   def create
     session[:form_params] = params.inspect
-    redirect_to new_student_path
+    redirect_to new_students_path
   end
 
 
