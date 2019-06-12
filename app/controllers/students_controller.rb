b32a63ba7ed775f1b6fbb98ec884e3d9fe992825
github username <github email address>
2019-06-12 11:23:04 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,18 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  #new method/paths for the new.html.erb form
+  def new #given to config/routes.rb
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    #find prefix needed using rake routes
+    #redirect_to prefix_path
+    redirect_to students_path
+    #have the form redirect back to the new view template ^ from readme
+  end
+  
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+  resources :students, only: [:index, :new, :create]
+    #resources :controller_name/folder_name, only: [:method/path, :method/path]
   get '/students/:id', to: 'students#show', as: 'student'
 end
