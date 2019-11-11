cdea9e94aba09a495d42bc1a4e50ffb032a7c219
jamieberrier <berrier.jamie@gmail.com>
2019-11-11 00:30:27 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -31,4 +31,6 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'capybara'
   gem 'database_cleaner'
+  gem 'pry'
+  gem 'tux'
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    # save params in session hash to access in view
+    session[:student_form_params] = params[:student]
+    redirect_to new_student_path
   end
 
 end
