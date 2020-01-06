bbdf2a496e9c23c87d5cd27d0203feebd7a9c015
github username <github email address>
2020-01-06 22:17:47 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :students, only: [:index, :new, :create]
+  # get '/student/:id', to: 'students#show', as: 'student'
+
   resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
+  get '/student/:id', to: 'students#show', as: 'student'
+  post '/students', to: 'students#create', as: 'create_student'
+  get '/students/new', to: 'students#new', as: 'new_student'
+
 end
