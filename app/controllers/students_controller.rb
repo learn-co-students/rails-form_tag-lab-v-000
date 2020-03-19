be0528dad2da10d7bd16af888b8efefff952327f
Tyler Sell <tylerselldesign@gmail.com>
2020-03-19 21:23:16 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,14 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path
+  end
+  
+  def new 
+    
+  end
 
 end
