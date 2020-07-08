9c4ae79f0a7c8719cf513fa0482f4b5ebae67932
yurigurlie207 <adrienne.yao@gmail.com>
2020-07-08 09:19:04 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -22,7 +22,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+group :development do
+  gem 'web-console'
+end
 
 group :development, :test do
   gem 'byebug'
@@ -31,4 +33,5 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'capybara'
   gem 'database_cleaner'
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,24 @@
 class StudentsController < ApplicationController
-  
+    before_action :set_student, only: :show
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def new
+    @student= Student.last
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path
+  end
+
+  private
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
