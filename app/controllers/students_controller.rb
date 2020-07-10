de7f2bfc6ce240d7c378ab5a674312991ef7d24c
dejarvzal <diannejarvisj@gmail.com>
2020-07-10 09:40:49 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,9 @@ class StudentsController < ApplicationController
   end
 
   def create
+    # binding.pry
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    session[:form_params] = params.inspect
     redirect_to new_student_path
   end
 
