169f5736a80520390aa6deed7771e037cb9ceab6
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2021-03-01 17:44:10 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+
+    redirect_to student_path(@student)
   end
 
 end
