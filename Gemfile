fe0c6fd19a633ba96af0a2fc632dbcb02061cc64
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2021-03-01 18:00:41 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 
 group :development, :test do
+  gem 'pry'
   gem 'byebug'
   gem 'spring'
   gem 'rspec-rails'
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,10 +12,9 @@ class StudentsController < ApplicationController
   end
 
   def create
-    binding.pry
     @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
     @student.save
 
     redirect_to student_path(@student)
